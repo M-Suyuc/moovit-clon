d0a6cf515443922ba3fcda5d52c0f726603b22db
marlon <marlonsuyuc@gmail.com>
2025-09-10 17:41:57 -0600

    refactor: comment out unused imports and adjust layout in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
-import { getStopInfo } from "@/actions/get-info";
+// import { getStopInfo } from "@/actions/get-info";
+import Link from "next/link";
+
 
 export default async function Home() {
-  const data = await getStopInfo()
-  console.log("🚀 ~ Home ~ data:", data.slice(0, 5))
-
+  // const data = await getStopInfo()
+  // console.log("🚀 ~ Home ~ data:", data.slice(0, 4))
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className="font-sans flex flex-col items-center justify-center min-h-screen p-8 pb-20 gap-5">
+      <Link href="/map" className="bg-blue-500 text-white py-2 px-5 rounded hover:bg-white hover:text-blue-500 hover:transition-colors">Go Map Leaflet</Link>
+
+      <Link href="/maplibregl" className="bg-blue-500 text-white py-2 px-5 rounded hover:bg-white hover:text-blue-500 hover:transition-colors">Go Map maplibregl</Link>
     </div>
   );
 }
