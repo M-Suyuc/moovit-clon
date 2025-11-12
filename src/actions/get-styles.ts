e261ab9c049b2bef2c0257d59d413b36e72e13dd
M-Suyuc <marlonsuyuc@gmail.com>
2025-11-11 19:03:50 -0600

    Remove GTFSService files  implementation and related methods from gtfsService

--- src/actions/get-styles.ts
+++
@@ -1,7 +0,0 @@
-"use server";
-
-export async function getStyle() {
-  const res = await fetch("http://localhost:8080/styles/osm-bright/style.json");
-  if (!res.ok) throw new Error("Failed to fetch style.json");
-  return res.json();
-}
