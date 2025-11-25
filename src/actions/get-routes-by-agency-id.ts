de6476b0b912cdd3c26de9a8e543ccea2a37ced5
M-Suyuc <marlonsuyuc@gmail.com>
2025-11-25 16:32:20 -0600

    feat: add getRoutesByAgencyId function to fetch routes details by ID

---
+++ src/actions/get-routes-by-agency-id.ts
@@ -0,0 +1,26 @@
+"use server";
+
+import db from "@/services/connect-db";
+
+export async function getRoutesByAgencyId({ agencyId }: { agencyId: string }) {
+  try {
+    const routes = await db.query(
+      `
+      SELECT 
+        route_id,
+        route_short_name,
+        route_long_name,
+        route_type
+      FROM routes
+      where agency_id = $1
+      ORDER BY route_short_name
+    `,
+      [`${agencyId}`]
+    );
+
+    return routes.rows;
+  } catch (error) {
+    console.error("Error fetching routes:", error);
+    return [];
+  }
+}
