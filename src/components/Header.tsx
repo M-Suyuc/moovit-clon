53c0c7ea9f46b9591ccd8df235e4b1589d7b6816
M-Suyuc <marlonsuyuc@gmail.com>
2025-11-27 17:34:32 -0600

    style: format Header component for improved readability

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,12 @@
 export const Header = () => {
   return (
-    <div className="container mx-auto h-16 border-b border-gray-300 mb-4 flex flex-row justify-between items-center">
-      <a href="/" className="text-2xl font-bold text-center md:text-3xl"><span className="text-sm md:text-lg">MLS</span>Transport</a>
-      <p className="text-center text-gray-400">Public Transit Routes and Stops</p>
+    <div className="container mx-auto h-16 border-b border-gray-300  flex flex-row justify-between items-center">
+      <a href="/" className="text-2xl font-bold text-center md:text-3xl">
+        <span className="text-sm md:text-lg">MLS</span>Transport
+      </a>
+      <p className="text-center text-gray-400">
+        Public Transit Routes and Stops
+      </p>
     </div>
-  )
-}
+  );
+};
